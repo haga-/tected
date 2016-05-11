42b038393ef5e5a1088571c74a5e850ef4d06933
haga <hagalmeida@inf.ufpel.edu.br>
2016-05-11 18:31:38 -0300

    Initial commit

---
+++ Pet/app/src/main/java/tected/pet/PetInfo.java
@@ -0,0 +1,19 @@
+package tected.pet;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hut8 on 5/11/16.
+ */
+public class PetInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pet_info);
+    }
+
+
+}
