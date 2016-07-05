b036e99b300446846c020a23a12aa9530eb9a8a6
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2016-07-05 15:54:07 -0300

    login só login sem logout...

--- Pet/app/src/main/java/tected/pet/MainActivity.java
+++ Pet/app/src/main/java/tected/pet/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.support.design.widget.TabLayout;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -15,10 +16,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.mikepenz.iconics.typeface.IIcon;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.holder.ImageHolder;
+import com.mikepenz.materialdrawer.holder.StringHolder;
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
@@ -34,12 +38,14 @@ import studios.codelight.smartloginlibrary.SmartCustomLoginListener;
 import studios.codelight.smartloginlibrary.SmartLoginBuilder;
 import studios.codelight.smartloginlibrary.SmartLoginConfig;
 import studios.codelight.smartloginlibrary.manager.UserSessionManager;
+import studios.codelight.smartloginlibrary.users.SmartFacebookUser;
+import studios.codelight.smartloginlibrary.users.SmartGoogleUser;
 import studios.codelight.smartloginlibrary.users.SmartUser;
 
 //http://cuidados-cachorro-gatos.webnode.com/rss/
 //http://www.procurasecachorro.com.br/blog/category/noticias/feed/
 
-public class MainActivity extends AppCompatActivity { //implements OnMapReadyCallback
+public class MainActivity extends AppCompatActivity implements SmartCustomLoginListener{ //implements OnMapReadyCallback
 
     //private GoogleMap mMap;
     private Drawer result = null;
@@ -47,7 +53,6 @@ public class MainActivity extends AppCompatActivity { //implements OnMapReadyCal
 
     public List<SmartUser> usuarios;
     SmartUser currentUser;
-    boolean gambiarraEstado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -106,44 +111,13 @@ public class MainActivity extends AppCompatActivity { //implements OnMapReadyCal
 
         final SmartLoginBuilder loginBuilder = new SmartLoginBuilder();
 
-        final SmartCustomLoginListener loginListener = new SmartCustomLoginListener() {
-            @Override
-            public boolean customSignin(SmartUser smartUser) {
-                Log.d("Main", "sign in");
-                for(int i = 0; i < usuarios.size(); i++){
-                    SmartUser user = usuarios.get(i);
-                    if(smartUser.equals(usuarios.get(i)))
-                        return true;
-                    if(smartUser.getUsername() == user.getUsername() && smartUser.getPassword() == user.getPassword())
-                        return true;
-                }
-                //return false;
-                Toast.makeText(getApplicationContext(), "Usuário não cadastrado", Toast.LENGTH_LONG).show();
-                return false;
-            }
 
-            @Override
-            public boolean customSignup(SmartUser smartUser) {
-                //do something with smartUser
-                Log.d("Main", "sign up");
-                return true;
-            }
-
-            @Override
-            public boolean customUserSignout(SmartUser smartUser) {
-                //do something with smartUser
-                UserSessionManager.logout(MainActivity.this, smartUser);
-                //acho que vai dar ruim
-                Log.d("Main", "sign out");
-                return true;
-            }
-        };
 
         currentUser = UserSessionManager.getCurrentUser(getApplicationContext());
         if(currentUser == null) {
             Intent intent = loginBuilder.with(getApplicationContext())
                     .isCustomLoginEnabled(true)
-                    .setSmartCustomLoginHelper(loginListener)
+                    .setSmartCustomLoginHelper(this)
                     .setAppLogo(R.mipmap.logo)
                     .build();
             startActivityForResult(intent, SmartLoginConfig.LOGIN_REQUEST);
@@ -282,7 +256,7 @@ public class MainActivity extends AppCompatActivity { //implements OnMapReadyCal
                                 if(id == 5) {
                                     Intent intent = loginBuilder.with(getApplicationContext())
                                             .isCustomLoginEnabled(true)
-                                            .setSmartCustomLoginHelper(loginListener)
+                                            .setSmartCustomLoginHelper(MainActivity.this)
                                             .build();
                                     startActivityForResult(intent, SmartLoginConfig.LOGIN_REQUEST);
                                     SmartUser user = UserSessionManager.getCurrentUser(getApplicationContext());
@@ -340,6 +314,42 @@ public class MainActivity extends AppCompatActivity { //implements OnMapReadyCal
         adapter.addFragment(new Timeline_fragment(), "Timeline");
         adapter.addFragment(new Mapa_fragment(), "Região");
         viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean customSignin(SmartUser smartUser) {
+        Log.d("Main", "sign in com implements");
+        for(int i = 0; i < usuarios.size(); i++){
+            SmartUser user = usuarios.get(i);
+            /*
+            Log.d("Main", "\"" + user.getUsername() + "\"  \""+user.getPassword()+"\"");
+            Log.d("Main", "\"" + smartUser.getUsername() + "\"  \""+smartUser.getPassword()+"\"");
+            Log.d("Main", String.valueOf(smartUser.getUsername().equals(user.getUsername())) +" "+ String.valueOf(smartUser.getPassword().equals(user.getPassword())));
+            */
+            if(smartUser.equals(usuarios.get(i)))
+                return true;
+            if(smartUser.getUsername().equals(user.getUsername()) && smartUser.getPassword().equals(user.getPassword()))
+                return true;
+        }
+        //return false;
+        Toast.makeText(getApplicationContext(), "Usuário não cadastrado", Toast.LENGTH_LONG).show();
+        return false;
+    }
+
+    @Override
+    public boolean customSignup(SmartUser smartUser) {
+        //do something with smartUser
+        Log.d("Main", "sign up com implements");
+        return true;
+    }
+
+    @Override
+    public boolean customUserSignout(SmartUser smartUser) {
+        //do something with smartUser
+        UserSessionManager.logout(MainActivity.this, smartUser);
+        //acho que vai dar ruim
+        Log.d("Main", "sign out com implements");
+        return true;
     }
 
     static class Adapter extends FragmentPagerAdapter {
